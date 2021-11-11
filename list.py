e6cef1661f3bd0369fdb0ee463b4393e4a0d2011
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-11-11 21:26:20 +0545

    List

--- list.py
+++ list.py
@@ -1,5 +1,20 @@
 #List: ordered, mutable, allows to duplicate elements
-myList = ["banana","cherry","apple","orange"]
-print(myList)
 
+mylist =[1,2,3,4,5,6,7,8,9]
+a = mylist[::2]
+print(a)
 
+list_org = ["banana","cherry","apple"]
+
+list_cpy = list_org
+
+list_cpy.append("lemon")
+
+print(list_cpy)
+print(list_org)
+
+a=[1,2,3,4,5,6,7,8,9]
+b = [i*i for i in a]
+
+print(a)
+print(b)
