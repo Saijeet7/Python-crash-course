f892b5399817e7fbfc33ebeb31da96f174d8129e
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-11-21 22:17:39 +0545

    Matlib

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+#Matlib
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+madlb = f"Computer programming is sp {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlb)
